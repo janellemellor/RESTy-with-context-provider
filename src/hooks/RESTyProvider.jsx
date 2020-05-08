3c791b2884911ca13c34ceeaa960c60d2357e60b
janelle mellor <janellemellor@gmail.com>
2020-05-07 17:18:32 -0700

    add notes

--- src/hooks/RESTyProvider.jsx
+++ src/hooks/RESTyProvider.jsx
@@ -5,10 +5,16 @@ import PropTypes from 'prop-types';
 const RESTyContext = createContext();
 
 export const RESTyProvider = ({ children }) => {
-  //set state
+  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
+  const [jsonBody, setJsonBody] = useState('');
+  const [response, setResponse] = useState('');
+
+  //add onChange that sets url, method, jsonBody
+  //add handleSubmit function that fetches from the service function and sets the response
   
   return (
-    <RESTyContext.Provider value={{ }} >
+    <RESTyContext.Provider value={{ url, method, jsonBody, response }} >
       {children}
     </RESTyContext.Provider>
   );
@@ -16,6 +22,11 @@ export const RESTyProvider = ({ children }) => {
 
 RESTyProvider.propTypes = {
   children: PropTypes.node
+};
+
+export const useUrlRequest = () => {
+  const { url } = useContext(RESTyContext);
+  return url;
 };
 
 
