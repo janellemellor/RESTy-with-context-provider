7893d0e7ac38144dc82400cde3963b6c47172c33
janelle mellor <janellemellor@gmail.com>
2020-05-09 12:24:47 -0700

    update form with provider

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Form from '../Form/Form';
+import Response from '../Response/Response';
+import { RESTyProvider } from '../../hooks/RESTyProvider';
 
 export default function App() {
-  return <Form />;
+  return (
+    <RESTyProvider >
+      <Form />
+      <Response />
+    </RESTyProvider>
+    
+  );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { RadioButtonGroup, RadioButton } from './RadioButton';
+import { useRESTy } from '../../hooks/RESTyProvider';
 
-const Form = ({ onSubmit, url, onChange, jsonBody }) => (
-  <form onSubmit={onSubmit}>
-    <fieldset>
-      <input name="url-input" type="text" value={url} onChange={onChange} />
-      <RadioButtonGroup name="methods" onuChange={onChange} >
-        <RadioButton value="GET" />
-        <RadioButton value="POST" />
-        <RadioButton value="PUT" />
-        <RadioButton value="PATCH" />
-        <RadioButton value="DELETE" />
-      </RadioButtonGroup>
-      <button>Go!</button>
-      <textarea name="jsonBody" value={jsonBody} onChange={onChange}> </textarea>
-    </fieldset>    
-  </form>
-);
+const Form = () => {
+  const { onSubmit, url, onChange, jsonBody } = useRESTy();
 
-Form.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  url: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  jsonBody: PropTypes.string,
+  return (
+    <form onSubmit={onSubmit}>
+      <fieldset>
+        <input name="url-input" type="text" value={url} onChange={onChange} />
+        <RadioButtonGroup name="methods" onuChange={onChange} >
+          <RadioButton value="GET" />
+          <RadioButton value="POST" />
+          <RadioButton value="PUT" />
+          <RadioButton value="PATCH" />
+          <RadioButton value="DELETE" />
+        </RadioButtonGroup>
+        <button>Go!</button>
+        <textarea name="jsonBody" value={jsonBody} onChange={onChange}> </textarea>
+      </fieldset>    
+    </form>
+  );
 };
 
 export default Form;
